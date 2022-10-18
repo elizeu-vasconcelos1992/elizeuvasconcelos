907e4ba988aaa8d1b8f1bf8f8913bc6afcb0831c
unknown <elizeucosta21@gmail.com>
2022-10-18 15:50:10 -0300

    refactor: remove item navbar

--- src/components/Header/NavBar/styles.ts
+++ src/components/Header/NavBar/styles.ts
@@ -33,6 +33,30 @@ export const StyledNav = styled.nav`
         pointer-events: none;
       }
     }
+
+    a {
+      width: 100%;
+      height: 6rem;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      padding: 0 0 0 4rem;
+      gap: 2vh;
+      cursor: pointer;
+      transition: 0.5s;
+      text-decoration: none;
+
+      svg {
+        min-width: 1.8rem;
+        min-height: 1.8rem;
+        pointer-events: none;
+      }
+
+      span {
+        font-size: 1.5vw;
+        pointer-events: none;
+      }
+    }
   }
 
   .current-effect {
@@ -59,6 +83,14 @@ export const StyledNav = styled.nav`
           font-size: calc(1.5vw + 3px);
         }
       }
+
+      a {
+        padding: 0 0 0 1.75rem;
+
+        span {
+          font-size: calc(1.5vw + 3px);
+        }
+      }
     }
   }
 
@@ -75,12 +107,28 @@ export const StyledNav = styled.nav`
         gap: 0.5vh;
         height: 3.5rem;
       }
+
+      a {
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        border-radius: 10px;
+        gap: 0.5vh;
+        height: 3.5rem;
+      }
     }
   }
 
   @media (max-width: 550px) {
     ul {
       li {
+        span {
+          font-size: calc(1.5vw + 4px);
+        }
+      }
+
+      a {
         span {
           font-size: calc(1.5vw + 4px);
         }
